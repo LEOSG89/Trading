02cda102420b4822b524376fd88e600ec3b4b9e7
LEOSG89 <leandrosoto89@yahoo.com>
2025-05-11 17:35:03 -0400

    config.py

--- config.py
+++ config.py
@@ -1,25 +1,29 @@
 # config.py
 import os
 
+# Base path del proyecto (carpeta donde está este archivo)
+BASE_DIR = os.path.dirname(__file__)
+
 # Carpeta donde se guardarán los archivos subidos
-UPLOADED_DIR = "uploaded_data"
+UPLOADED_DIR = os.path.join(BASE_DIR, "uploaded_data")
 
 # Ruta al índice JSON dentro de la carpeta anterior
 UPLOADED_INDEX = os.path.join(UPLOADED_DIR, "index.json")
 
 # Rutas y parámetros globales
-COL_FILE = 'col_names.json'
+COL_FILE   = 'col_names.json'
 TABLE_FILE = 'table_config.json'
 
 # Columnas fijas por defecto en el DataFrame
 FIXED_COLS = [
     'Activo', 'C&P', 'D', 'Día', 'Fecha / Hora',
-    'Fecha / Hora de Cierre', '#Cont', 'STRK Buy', 'STRK Sell', 'Deposito', 'Retiro', 'Profit'
+    'Fecha / Hora de Cierre', '#Cont', 'STRK Buy',
+    'STRK Sell', 'Deposito', 'Retiro', 'Profit'
 ]
 
 # Lista de activos disponibles
 ASSETS = [
     'AMZN','AAPL','GOOG','TSLA','MSFT','META','NFLX',
-    'AMD','MU','QCOM','NVDA','AVGO','TSM','SPY','QQQ','DIA','IWM',
-    'DEP','RET'
+    'AMD','MU','QCOM','NVDA','AVGO','TSM','SPY','QQQ',
+    'DIA','IWM','DEP','RET'
 ]
